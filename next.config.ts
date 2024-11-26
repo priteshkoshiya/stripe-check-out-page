c55425c59afdede935a35fcc844f2478c41129c2
priteshkoshiya <koshiyapritesh21@gmail.com>
2024-11-26 20:54:04 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone', // Ensures it creates a single deployable build
   distDir: 'build',     // Sets the output directory to './build'
 };
